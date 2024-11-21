94fbf76094805f9dce8a98b30f0c24fa913e732b
CRVanware <crodriguez@vanware.io>
2024-11-21 20:23:33 +0000

    versión 0.1

---
+++ src/js/icons.js
@@ -0,0 +1,12 @@
+// import fontawesome from '/workspaces/spain-fs-pt-85-simple-counter-crodriguez/node_modules/@fortawesome/fontawesome-free';
+
+// import faClock from '/workspaces/spain-fs-pt-85-simple-counter-crodriguez/node_modules/@fortawesome/fontawesome-free/js/regular.js';
+// fontawesome.config = {
+//     autoReplaceSvg: 'nest'
+// };
+import fontawesome from '@fortawesome/fontawesome';
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,59 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import PropTypes from 'prop-types';
+
+import "./icons.js";
+import 'bootstrap';
+import '../styles/index.css';
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+
 
 //import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return (
+        <div className="fondoClock">
+            <div className="clock">
+                <i clasname="far fa-clock"></i>
+            </div>
+            <div className="six">{props.digitSix % 10}</div>
+            <div className="five">{props.digitFive % 10}</div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitThree % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+        </div>
+    )
+}
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+SimpleCounter.propTypes = {
+    digitOne: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitSix: PropTypes.number,
+};
+
+let contador = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+setInterval (function() {
+    const six = Math.floor(contador/100000);
+    const five = Math.floor(contador/10000);
+    const four = Math.floor(contador/1000);
+    const three = Math.floor(contador/100);
+    const two = Math.floor(contador/10);
+    const one = Math.floor(contador/1);
+    contador++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />
+    );
+},1000);
+
+
+
+
 
